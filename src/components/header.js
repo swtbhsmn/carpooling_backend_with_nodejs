b217879b7792138a6c6cff7bb651c92a144c4e5b
Swetabh suman <swtbhsmn@gmail.com>
2020-10-29 17:13:06 +0530

    navWork when user login . It goes to logout part.

--- src/components/header.js
+++ src/components/header.js
@@ -9,7 +9,7 @@ import {
     NavLink,
     NavbarText,
     Button,
-    Modal,ModalBody
+    Modal, ModalBody
 } from 'reactstrap';
 import Login from "./login";
 class Header extends React.Component {
@@ -18,14 +18,15 @@ class Header extends React.Component {
         this.state = {
             isOpen: false,
             isLoginOpen: false,
-            user:{
-                username:"",
-                password:""
+            user: {
+                username: "",
+                password: ""
             }
         }
-     
+
         this.toggleButton.bind(this.toggleButton);
         this.toggleLoginButton.bind(this.toggleLoginButton);
+        this.logoutButton.bind(this.logoutButton);
     }
 
     toggleButton = () => {
@@ -34,14 +35,15 @@ class Header extends React.Component {
     toggleLoginButton = () => {
         this.setState({ isLoginOpen: !this.state.isLoginOpen });
     }
-
+    logoutButton = ()=>{
+    window.localStorage.removeItem("user");
+    this.props.redirect.history.push('/home');
+    }
     render() {
-        return (
-            <div className="">
+        const Toolnav = () => {
 
-                <Navbar className="navbar-fixed-top" color="light" light expand="md">
-                    <NavbarBrand href="/">CarPooling</NavbarBrand>
-                    <NavbarToggler onClick={this.toggleButton} />
+            if (window.localStorage.getItem("user") === null) {
+                return (
                     <Collapse isOpen={this.state.isOpen} navbar>
                         <Nav className="mr-auto" navbar>
 
@@ -49,23 +51,52 @@ class Header extends React.Component {
                                 <NavLink href="/components/">About</NavLink>
                             </NavItem>
 
-                      
-                      
+
+
                         </Nav>
-                        <NavbarText><Button  style={{cursor:"pointer",background:"#fff",color:"#000",marginRight:"5px"}} onClick={this.toggleLoginButton}>Signup</Button></NavbarText>
-                
-                        <NavbarText><Button  style={{cursor:"pointer",background:"#fff",color:"#000"}} onClick={this.toggleLoginButton}>Login</Button></NavbarText>
+                        <NavbarText><Button style={{ cursor: "pointer", background: "#fff", color: "#000", marginRight: "5px" }} onClick={this.toggleLoginButton}>Signup</Button></NavbarText>
+
+
+                        <NavbarText><Button style={{ cursor: "pointer", background: "#fff", color: "#000" }} onClick={this.toggleLoginButton}>Login</Button></NavbarText>
                     </Collapse>
+                );
+            }
+            else {
+                return (
+                    <Collapse isOpen={this.state.isOpen} navbar>
+                        <Nav className="mr-auto" navbar>
+
+                            <NavItem>
+                                <NavLink href="/components/">About</NavLink>
+                            </NavItem>
+
+
+
+                        </Nav>
+
+
+                        <NavbarText><Button style={{ cursor: "pointer", background: "#fff", color: "#000" }} onClick={this.logoutButton}>Logout</Button></NavbarText>
+                    </Collapse>
+                );
+            }
+        }
+        return (
+            <div className="">
+
+                <Navbar className="navbar-fixed-top" color="light" light expand="md">
+                    <NavbarBrand href="/">CarPooling</NavbarBrand>
+                    <NavbarToggler onClick={this.toggleButton} />
+                    <Toolnav />
                 </Navbar>
-                <Modal isOpen={this.state.isLoginOpen} toggle={this.toggleLoginButton} style={{top:"25%"}} >
-               <div style={{width:"100%"}} onClick={this.toggleLoginButton}><i style={{cursor:"pointer",float:"right"}} className="material-icons">close</i></div>
+                <Modal isOpen={this.state.isLoginOpen} toggle={this.toggleLoginButton} style={{ top: "25%" }} >
+                    <div style={{ width: "100%" }} onClick={this.toggleLoginButton}><i style={{ cursor: "pointer", float: "right" }} className="material-icons">close</i></div>
                     <ModalBody>
-                       
+
                         <Login toggleLoginButton={this.toggleLoginButton} props={this.props.redirect} />
-                      
-                       
+
+
                     </ModalBody>
-                 
+
                 </Modal>
             </div>
         )
